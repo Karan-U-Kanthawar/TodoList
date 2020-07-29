c07c624f61115938919cdb9bfb7b3eb77303807c
Karan-U-Kanthawar <karankanthawar@gmail.com>
2020-07-29 18:18:02 +0530

    implemented "Add a todo"

--- TODO.js
+++ TODO.js
@@ -1,6 +1,16 @@
 const add = document.querySelectorAll('.add');
+const ulist = document.querySelectorAll('.todos');
 
-add.addEventListener('submit', (event) => {
-  event.preventDefualt();
-  console.log(event.value);
+add[0].addEventListener('submit', (event) => {
+  event.preventDefault();
+  const newTodo = event.target.add.value.trim();
+  const newHTML = `<li class="list-group-item d-flex justify-content-between align-items-center">
+                      <span>${newTodo}</span>
+                      <i class="far fa-trash-alt delete"></i>
+                  </li>`;
+
+  if (newTodo.length) {
+    ulist[0].innerHTML += newHTML;
+    add[0].reset();
+  }
 });
